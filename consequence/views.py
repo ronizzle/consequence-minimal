7b1f9bebec351a642c11dec731c8c27cf344814d
Roniel Bernas <roniel@eligeo.com>
2021-05-31 05:47:43 +0800

    Implemented user registration

--- consequence/views.py
+++ consequence/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
 def index(request):
     return render(request, 'consequence/index.html')
 
+
 def register(request):
-    return render(request, 'consequence/register.html')
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'consequence/register.html', context)
