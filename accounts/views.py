ffeea8b897d1d13ab71394ed3172b0009d746d65
Roniel Bernas <roniel@eligeo.com>
2021-05-30 21:51:37 +0800

    Learned how to use imported URLS

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('home page')
+
+
+def products(request):
+    return HttpResponse('products page')
+
+
+def customer(request):
+    return HttpResponse('customer page')
--- consequence_minimal/urls.py
+++ consequence_minimal/urls.py
@@ -14,21 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.http import HttpResponse
 
 
-def home(request):
-    return HttpResponse('Home page')
-
-
-def contact(request):
-    return HttpResponse('Contact  page')
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('contact', contact),
+    path('', include('accounts.urls')),
 ]
