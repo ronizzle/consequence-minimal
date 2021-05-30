a1aa9204d031f46fded4f12d7eb43cf133693665
Roniel Bernas <roniel@eligeo.com>
2021-05-30 21:57:55 +0800

    Learned about rendering templates

--- accounts/views.py
+++ accounts/views.py
@@ -4,12 +4,12 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('home page')
+    return render(request, 'accounts/dashboard.html')
 
 
 def products(request):
-    return HttpResponse('products page')
+    return render(request, 'accounts/products.html')
 
 
 def customer(request):
-    return HttpResponse('customer page')
+    return render(request, 'accounts/customer.html')
