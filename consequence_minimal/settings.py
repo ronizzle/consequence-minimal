2676501de8cb0bb827031af40b723a32e7875c58
Roniel Bernas <roniel@eligeo.com>
2021-06-01 23:59:30 +0800

    Changed Line Separators from CRLF to LF

--- consequence_minimal/settings.py
+++ consequence_minimal/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'consequence_minimal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'consequence_demo_minimal',
+        'USER': 'postgres',
+        'PASSWORD': 'allstar',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
